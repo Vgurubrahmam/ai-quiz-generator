db1c4e2b0bc8adc995dacb668065bed59f81dafe
gurubrahmam <vgurubrahmam338@gmail.com>
2025-11-08 10:09:51 +0530

    updated middleware

--- Backend/app.py
+++ Backend/app.py
@@ -33,7 +33,7 @@ app = FastAPI(title="AI wiki quiz generator API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://aiquizmaker-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
